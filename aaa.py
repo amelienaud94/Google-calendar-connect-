17e8b2da4d4e51fca8374a111e47ed30005bc8a3
amelienaud94 <amelie.naudascher@hec.edu>
2016-10-23 16:12:09 +0200

    Update aaa.py
    Update

--- aaa.py
+++ aaa.py
@@ -1,12 +1,94 @@
-import requests
+
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+import datetime
+
 import urllib
-adresseDepart="48 Boulevard des Batignolles, 75017 Paris "
-print(urllib.quote(adresseDepart))
-rcoordDepart=requests.get("http://maps.googleapis.com/maps/api/geocode/json?address="+urllib.quote(adresseDepart)+"&sensor=false")
-print(rcoordDepart.json()['results'][0]['geometry']['location']);
+import requests
 
-r=requests.get("https://developer.citymapper.com/api/1/traveltime/?startcoord=51.525246%2C0.084672&endcoord=51.559098%2C0.074503&time=2014-11-06T19%3A00%3A02-0500&time_type=arrival&key=a5e47d7d91fa9dc49610d5ffca14fdea")
-print(r.text);
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
 
-coord=requests.get("http://maps.googleapis.com/maps/api/geocode/json?address=546%20rue%20Baruch%20de%20Spinoza,%20Avignon&sensor=false")
-print(coord.json()['results'][0]['geometry']['location']);
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/calendar-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+
+    Creates a Google Calendar API service object and outputs a list of the next
+    10 events on the user's calendar.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 1 event')
+    eventsResult = service.events().list(
+        calendarId='primary', timeMin=now, maxResults=1, singleEvents=True,
+        orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        computeAlarmTime(start, event['location'])
+
+def computeAlarmTime(startTime, location):
+    adresseDepart="48 Boulevard des Batignolles, 75017 Paris "
+    print(urllib.quote(adresseDepart))
+    rcoordDepart=requests.get("http://maps.googleapis.com/maps/api/geocode/json?address="+urllib.quote(adresseDepart)+"&sensor=false")
+    print(rcoordDepart.json()['results'][0]['geometry']['location']);
+
+    r=requests.get("https://developer.citymapper.com/api/1/traveltime/?startcoord=51.525246%2C0.084672&endcoord=51.559098%2C0.074503&time=2014-11-06T19%3A00%3A02-0500&time_type=arrival&key=a5e47d7d91fa9dc49610d5ffca14fdea")
+    print(r.text);
+
+    coord=requests.get("http://maps.googleapis.com/maps/api/geocode/json?address=546%20rue%20Baruch%20de%20Spinoza,%20Avignon&sensor=false")
+    print(coord.json()['results'][0]['geometry']['location']);
+
+
+if __name__ == '__main__':
+    main()
